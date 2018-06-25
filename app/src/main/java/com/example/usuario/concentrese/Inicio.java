123d8b681c293432979b2c06969d2618257431ff
axel8526 <casanova8526@gmail.com>
2018-06-25 10:00:07 -0500

    llll

---
+++ app/src/main/java/com/example/usuario/concentrese/Inicio.java
@@ -0,0 +1,93 @@
+package com.example.usuario.concentrese;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Inicio extends AppCompatActivity {
+
+    private Dialog escojeDificultad;
+    private int[] dificultad;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+        escojeDificultad=new Dialog(this);
+        escojeDificultad.setContentView(R.layout.escoje_dificultad_mensaje);
+        escojeDificultad.setCanceledOnTouchOutside(false);
+        escojeDificultad.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dificultad=new int[2];
+    }
+
+    public void botones(View v){
+        Intent i;
+
+
+        switch (v.getId()){
+
+            case R.id.jugar:
+
+                escojeDificultad.show();
+
+
+                break;
+            case R.id.ajustes:
+                i=new Intent(this, Configura.class);
+                startActivity(i);
+
+                break;
+            case R.id.puntajes:
+                break;
+
+
+            case R.id.facil:
+
+                dificultad[0]=8;
+                dificultad[1]=4;
+
+                i=new Intent(this, Partida.class);
+                i.putExtra("capacidad",dificultad[0]);
+                i.putExtra("parejas",dificultad[1]);
+                startActivity(i);
+                break;
+            case R.id.medio:
+                dificultad[0]=12;
+                dificultad[1]=6;
+
+                i=new Intent(this, Partida.class);
+
+                i.putExtra("capacidad",dificultad[0]);
+                i.putExtra("parejas",dificultad[1]);
+                startActivity(i);
+                break;
+            case R.id.dificil:
+                dificultad[0]=16;
+                dificultad[1]=8;
+
+                i=new Intent(this, Partida.class);
+
+                i.putExtra("capacidad",dificultad[0]);
+                i.putExtra("parejas",dificultad[1]);
+                startActivity(i);
+                break;
+
+        }
+
+    }
+    public void onResume(){
+        super.onResume();
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/usuario/concentrese/ItemCarta.java
@@ -0,0 +1,35 @@
+package com.example.usuario.concentrese;
+
+/**
+ * Created by Usuario on 17/06/2018.
+ */
+
+public class ItemCarta {
+
+    private int numero;
+    private int fondoImagenL;
+
+
+
+    public ItemCarta(int n, int fondoL){
+        numero=n;
+        fondoImagenL=fondoL;
+
+    }
+
+    public void setFondoImagenL(int fondoImagenL) {
+        this.fondoImagenL = fondoImagenL;
+    }
+
+    public int getFondoImagenL() {
+        return fondoImagenL;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+}
